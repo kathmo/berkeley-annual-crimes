6bb4710866f17b3c2aea8d7fb0120227fdee7197
Kathryn <kathmo@users.noreply.github.com>
2016-12-03 14:38:21 -0800

    Graph line graph of murder manslaughter data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
-// var ctx = $("#graph");
-var ctx = document.getElementById("graph");
-
 $.getJSON("https://data.cityofberkeley.info/api/views/efkp-2py4/rows.json", function (JSONdata) {
     var dataArr = JSONdata.data;
-    console.log(dataArr);
+    var yearLabels = [];
+    var murdMansData = [];
+
     console.log(dataArr[0][8]);
+
+    for (var i = 0; i < (dataArr.length - 1); i++) {
+        yearLabels.push(dataArr[i][8]);
+        murdMansData.push(dataArr[i][9]);
+    }
+
+    console.log(yearLabels[5]);
+    console.log(murdMansData[5]);
+
+    var graphData = {
+        labels: yearLabels,
+        datasets: [
+            {
+                label: "Murder and Manslaughter",
+                data: murdMansData
+            }
+        ]
+    };
+
+    var ctx = $("#graph");
+    var graph = new Chart(ctx, {
+        type: 'line',
+        data: graphData
+    });
 });
