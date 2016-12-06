c1c30f7c23f722af95815d2dfa8478cfcb2e00c1
Kathryn <kathmo@users.noreply.github.com>
2016-12-05 23:47:05 -0800

    Graph data for seven types of crimes on separate line graphs

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,10 @@ $.getJSON("https://data.cityofberkeley.info/api/views/efkp-2py4/rows.json", func
     var murdMansData = [];
     var rapeData = [];
     var aggrAssaultData = [];
-    /*
     var robData = [];
     var burglData = [];
     var larceTheftData = [];
     var mvTheftData = [];
-    */
     //var totCrimesArr = [];
 
     for (var i = 0; i < (crimesData.length); i++) {
@@ -17,12 +15,10 @@ $.getJSON("https://data.cityofberkeley.info/api/views/efkp-2py4/rows.json", func
         murdMansData.push(crimesData[i][9]);
         rapeData.push(crimesData[i][10]);
         aggrAssaultData.push(crimesData[i][12]);
-        /*
         robData.push(crimesData[i][11]);
         burglData.push(crimesData[i][13]);
         larceTheftData.push(crimesData[i][14]);
         mvTheftData.push(crimesData[i][15]);
-        */
         //totCrimesArr.push(crimesData[i][16]);
     }
 
@@ -56,6 +52,46 @@ $.getJSON("https://data.cityofberkeley.info/api/views/efkp-2py4/rows.json", func
         ]
     };
 
+    var robChartData = {
+        labels: yearLabels,
+        datasets: [
+            {
+                label: "Robbery",
+                data: robData
+            }
+        ]
+    };
+
+    var burglChartData = {
+        labels: yearLabels,
+        datasets: [
+            {
+                label: "Burglary",
+                data: burglData
+            }
+        ]
+    };
+
+    var larceTheftChartData = {
+        labels: yearLabels,
+        datasets: [
+            {
+                label: "Theft (Larceny)",
+                data: larceTheftData
+            }
+        ]
+    };
+
+    var mvTheftChartData = {
+        labels: yearLabels,
+        datasets: [
+            {
+                label: "Theft (Motor Vehicle)",
+                data: mvTheftData
+            }
+        ]
+    };
+
     Chart.defaults.global.responsive = false;
     Chart.defaults.global.title.display = true;
     Chart.defaults.global.elements.line.tension = 0;
@@ -65,12 +101,10 @@ $.getJSON("https://data.cityofberkeley.info/api/views/efkp-2py4/rows.json", func
     var murdMansCtx = $("#murd-mans-graph");
     var rapeCtx = $("#rape-graph");
     var aggrAssaultCtx = $("#aggr-assault-graph");
-    /*
     var robCtx = $("#rob-graph");
     var burglCtx = $("#burgl-graph");
     var larceTheftCtx = $("#larce-theft-graph");
     var mvTheftCtx = $("#mv-theft-graph");
-    */
 
     var xAxesOptions = [{
         scaleLabel: {
@@ -122,25 +156,6 @@ $.getJSON("https://data.cityofberkeley.info/api/views/efkp-2py4/rows.json", func
         data: aggrAssaultChartData,
         options: {
             title: {
-                text: 'Rape'
-            },
-            scales: {
-                xAxes: xAxesOptions,
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Number of Rapes'
-                    }
-                }]
-            }
-        }
-    });
-
-    var aggrAssaultChart = new Chart(aggrAssaultCtx, {
-        type: 'line',
-        data: aggrAssaultChartData,
-        options: {
-            title: {
                 text: 'Aggravated Assault'
             },
             scales: {
@@ -149,6 +164,82 @@ $.getJSON("https://data.cityofberkeley.info/api/views/efkp-2py4/rows.json", func
                     scaleLabel: {
                         display: true,
                         labelString: 'Number of Aggravated Assaults'
+                    }
+                }]
+            }
+        }
+    });
+
+    var robChart = new Chart(robCtx, {
+        type: 'line',
+        data: robChartData,
+        options: {
+            title: {
+                text: 'Robbery'
+            },
+            scales: {
+                xAxes: xAxesOptions,
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Number of Robberies'
+                    }
+                }]
+            }
+        }
+    });
+
+    var robChart = new Chart(burglCtx, {
+        type: 'line',
+        data: burglChartData,
+        options: {
+            title: {
+                text: 'Burglary'
+            },
+            scales: {
+                xAxes: xAxesOptions,
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Number of Burglaries'
+                    }
+                }]
+            }
+        }
+    });
+
+    var larceTheftChart = new Chart(larceTheftCtx, {
+        type: 'line',
+        data: larceTheftChartData,
+        options: {
+            title: {
+                text: 'Theft (Larceny)'
+            },
+            scales: {
+                xAxes: xAxesOptions,
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Number of Thefts (Larcenies)'
+                    }
+                }]
+            }
+        }
+    });
+
+    var mvTheftChart = new Chart(mvTheftCtx, {
+        type: 'line',
+        data: mvTheftChartData,
+        options: {
+            title: {
+                text: 'Theft (Motor Vehicle)'
+            },
+            scales: {
+                xAxes: xAxesOptions,
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Number of Thefts (Motor Vehicles)'
                     }
                 }]
             }
